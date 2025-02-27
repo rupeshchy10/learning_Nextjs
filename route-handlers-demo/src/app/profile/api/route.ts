4d45fe9403f21203eb9379817590fa5d6810bccc
rupeshchy10 <rupevilary1010@gmail.com>
2025-02-27 11:58:31 +0545

    Implement PATCH and DELETE methods for comments; enhance GET method with query filtering and update profile API to handle headers and cookies

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,24 @@
-export async function GET() {
-	return new Response("Profile API data");
+import { headers, cookies } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+	const requestHeaders = new Headers(request.headers);
+	console.log(requestHeaders.get("Authorization"));
+
+	const headersList = await headers();
+	console.log(headersList.get("Authorization"));
+
+	const theme = request.cookies.get("theme");
+	console.log(theme);
+
+	const cookieStore = await cookies();
+	cookieStore.set("resultsPerPage","20");
+	console.log(cookieStore.get("resultsPerPage"));
+
+	return new Response("<h1>Profile API data</h1>", {
+		headers: {
+			"Content-Type": "text/html",
+			"Set-Cookie": "theme=dark",
+		},
+	});
 }
